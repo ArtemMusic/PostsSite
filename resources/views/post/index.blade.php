d2920465172a25021c4cfab5a3e0a709e43d7d7b
Artem Muzychuk <50505906+ArtemMusic@users.noreply.github.com>
2022-10-02 12:51:32 +0300

    feat: Добавил readme, подготовил к релизу Измменил всё на русский язык, переделал кнопки, изменил неработающие ссылки, визуально улучшил сайт.

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,14 +4,19 @@
 @foreach($posts as $post)
 <div class="allposts"><a href="{{route('post.show', $post->id)}}">{{$count++}}. ({{$post->id}}) {{$post->title}}</a></div>
 @endforeach
+@can('view', auth()->user())
+<br>
+<br>
+<div class="btn_post_admin">
+    <form action="admin/posts">
+        <button class="btn btn-primary">АдминПанель</button>
+    </form>
+</div>
+@endcan
+<form action="/login">
+    <button class="btn btn-primary">Аккаунт</button>
+</form>
 <div>
     {{$posts->withQueryString()->links()}}
 </div>
-@can('view', auth()->user())
-<div class="btn_post_admin">
-    <a href="/admin/posts"><- АдминПанель</a>
-    <br>
-</div>
-    <a href="/login"><- Аккаунт</a>
-@endcan
 @endsection
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,52 +1,52 @@
 @extends('layouts.main')
 @section('content')
-    <h1>{{$id->title}}</h1>
-    <p>{{$id->content}}</p>
-    <p><b>Images: </b>{{$id->images}}</p>
-    <!-- {{$my_category = ""}} -->
-    <!-- @foreach($categories as $categ)
+<h1>{{$id->title}}</h1>
+<p>{{$id->content}}</p>
+<p><b>Images: </b>{{$id->images}}</p>
+<!-- {{$my_category = ""}} -->
+<!-- @foreach($categories as $categ)
         {{$id->category_id === $categ->id ? $my_category = $categ->title : "Без категории"}}
     @endforeach -->
-    <p><b>Category: </b>{{$my_category}}</p>
-    <!-- {{$my_tag = ""}} -->
-    <!-- {{$title_tag = ""}} -->
-    <p><b>Tags: </b>
+<p><b>Category: </b>{{$my_category}}</p>
+<!-- {{$my_tag = ""}} -->
+<!-- {{$title_tag = ""}} -->
+<p><b>Tags: </b>
     @foreach($pt as $p_t)
-        <!-- {{$id->id == $p_t->post_id ? $my_tag = $p_t->tag_id : ""}} -->
-        @foreach($tags as $tag)
-            {{$my_tag == $tag->id ? $title_tag = $tag->title . ',' : ""}}
-        @endforeach
-    @endforeach</p>
-    
-    <p><b>Status: </b>
-        @if($id->status == 1)
-            Опубликован
-        @else
-            Скрыт
-        @endif</p>
-    <p><b>Deleted: </b>
-        @if($id->deleted_at == NULL)
-            Существует
-        @else
-            Удален
-        @endif
-    </p>
-    @can('view', auth()->user())
-    <a href="{{route('post.edit', $id)}}">Редактировать</a>
-    <br><br>
-    <form action="{{route('post.destroy', $id)}}" method="POST">
-        @csrf
-        @method("delete")
-        <button type="submit" class="btn btn-danger" href="{{route('post.destroy', $id->id)}}">Удалить</button>
-    </form>
-    @endcan
-    <a href="{{route('post.index')}}">Назад</a>
-    @can('view', auth()->user())
-    <br>
-    <br>
-    <div class="btn_post_admin">
-    <a href="/admin/posts"><- АдминПанель</a>
-    <br>
-    @endcan
+    <!-- {{$id->id == $p_t->post_id ? $my_tag = $p_t->tag_id : ""}} -->
+    @foreach($tags as $tag)
+    {{$my_tag == $tag->id ? $title_tag = $tag->title . ',' : ""}}
+    @endforeach
+    @endforeach
+</p>
+
+<p><b>Status: </b>
+    @if($id->status == 1)
+    Опубликован
+    @else
+    Скрыт
+    @endif</p>
+<p><b>Deleted: </b>
+    @if($id->deleted_at == NULL)
+    Существует
+    @else
+    Удален
+    @endif
+</p>
+@can('view', auth()->user())
+<form action="{{route('post.edit', $id)}}">
+    <button class="btn btn-secondary">Редактировать</button>
+</form>
+<br><br>
+<form action="{{route('post.destroy', $id)}}" method="POST">
+    @csrf
+    @method("delete")
+    <button type="submit" class="btn btn-danger" href="{{route('post.destroy', $id->id)}}">Удалить</button>
+</form>
+@endcan
+<form action="{{route('post.index')}}">
+    <button class="btn btn-primary">Назад</button>
+</form>
+@can('view', auth()->user())
+@endcan
 </div>
 @endsection
